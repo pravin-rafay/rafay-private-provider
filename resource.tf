92746f035ec15980d256c19eaa1b08450a711b53
Pravin Tatti <pravin@rafay.co>
2024-03-05 06:37:05 +0530

    added credentials back

--- resource.tf
+++ resource.tf
@@ -8,8 +8,8 @@ resource "rafay_workload_cd_operator" "operator-demo" {
     repo_url        = "https://github.com/stephan-rafay/test-tfcd.git"
     repo_branch     = "main"
      credentials {
-      username = ""
-      token = ""
+      username = "stephan-rafay"
+      token = "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"
     }
 
     path_match_pattern = "/:project/:namespace/:workload"
