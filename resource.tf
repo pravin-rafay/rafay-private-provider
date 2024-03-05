4bc9b6b829aa5277f4125d775e06954363809286
Pravin Tatti <pravin@rafay.co>
2024-03-05 10:58:01 +0530

    Fixed the workload to clone repo in /tmp/ directory

--- resource.tf
+++ resource.tf
@@ -4,13 +4,13 @@ resource "rafay_workload_cd_operator" "operator-demo" {
     project = "benny"
   }
   spec {
-    repo_local_path = "/opt/rafay/application-repo"
+    repo_local_path = "/tmp/application-repo"
     repo_url        = "https://github.com/stephan-rafay/test-tfcd.git"
     repo_branch     = "main"
-     credentials {
-      username = "stephan-rafay"
-      token = "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"
-    }
+#     credentials {
+#      username = "stephan-rafay"
+#      token = "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"
+#    }
 
     path_match_pattern = "/:project/:namespace/:workload"
     base_path = "common"
